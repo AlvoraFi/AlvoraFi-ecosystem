3f34bb2e6db3c7283a42fa3517e0b9d20107aa0d
AlvoraFi <wadbaga7@gmail.com>
2026-06-26 13:06:16 +0700

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Hero from "@/components/Hero";
+import Features from "@/components/Features";
+import WalletCard from "@/components/WalletCard";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Features />
+      <WalletCard />
+    </>
+  );
+}
